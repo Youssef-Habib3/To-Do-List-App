092946a8ee47752f196108785eae2fd9d3713c92
Youssef Habib <yousefhabibelsehamy33@gmail.com>
2024-08-11 17:32:05 +0300

    motion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,21 @@
+"use client";
+
 import Header from "@/components/Header";
 import Main from "@/components/Main";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <div className="w-[100vw] h-[100vh] flex items-center justify-center flex-col">
-      <div className="bg-white rounded-lg text-black px-5 py-3">
+      <motion.div
+        initial={{ opacity: 0, y: "-100%" }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 1, type: "spring", mass: 2 }}
+        className="bg-white rounded-lg text-black px-5 py-3"
+      >
         <Header />
         <Main />
-      </div>
+      </motion.div>
     </div>
   );
 };
